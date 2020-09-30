bdaede5778dae0d0b9cce1041afb790e8a211976
bradykaban <bradyendakaban@gmail.com>
2020-09-30 21:37:30 +0700

    Commit HelloDts class main

---
+++ src/com/digitalent/hellodts/pemula/Main.java
@@ -0,0 +1,7 @@
+package com.digitalent.hellodts.pemula;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! Hello DTS! I am Brady.");
+    }
+}
